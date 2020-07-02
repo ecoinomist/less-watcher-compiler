9dc4880ec538283a6bb4b7ab884bf38bbc3ee2c4
Chris Co <coswarm@gmail.com>
2020-07-02 21:15:00 +0300

    update to monorepo setup

--- README.md
+++ README.md
@@ -14,69 +14,9 @@ yarn add -D less-watcher-compiler
 ```
 
 ### Options
-Create less-watcher-compiler.config.js (optional) in the root directory with this content:
-```js
-module.exports = function (defaultConfig) {
-  return {
-    ...defaultConfig,
-    // Default Options
-    files: [ // list of source files to watch for changes and corresponding tasks to run
-      {
-        task: 'css', // name of the task to run, must be one of ['css', 'copy']
-        watch:  [ // file/s to watch for changes, using glob pattern
-          'style/**/*.less',
-          'style/override/**/*',
-          'style/theme.config'
-        ],
-        compile: 'style/all.less', // file/s to compile when changes detected, using glob pattern
-        output: 'public/static/', // destination directory where new file/s should be saved
-      },
-      {
-        task: 'copy', // copy over `compile` file/s to `output` directory without compilation
-        watch:  [
-          'style/fonts/**/*', // watch for changes of all file types within `style/fonts/` directory
-        ],
-        compile: 'style/fonts/**/*.{eot,eof,svg,ttf,woff,woff2}', // only copy over files with matched extensions
-        output: 'public/static/fonts/',
-        renameOptions: {dirname: ''}, // make folder structure flat on output, using `gulp-rename` npm package
-      },
-    ],
-    symlinks: [ // useful for Semantic UI theme.config setup using `semantic-ui-less` library
-      {
-        // paths are relative to process.cwd()
-        target: 'style/theme.config', // path to the source file to reference
-        link: 'node_modules/semantic-ui-less/theme.config' // path to the file that references the target
-      }
-    ],
-    minify: false,
-    sourcemap: false,
-    autoprefix: true,
-    flexbugsfix: true,
-    browserslist: {
-      "production": [
-        ">0.3%",
-        "not dead",
-        "not op_mini all"
-      ],
-      "development": [
-        "last 1 chrome version",
-        "last 1 firefox version",
-        "last 1 safari version",
-        "last 1 ie version"
-      ]
-    },
-    postcssPlugins: [], // additional PostCSS plugins to use (you must install them yourself)
-    postcssOptions: undefined, // additional PostCSS options to use (i.e. `{ parser: 'sugarss' }`)
-    plugins: [
-      'less-plugin-glob',
-      'less-plugin-functions',
-    ],
-    // ...Other options passed to `lessc` compiler (currently using `gulp-less` npm package)
-  }
-}
-```
-All options can be passed as command line (cli) arguments, which will override config file options.
+Create `less-watcher-compiler.config.js` (optional) in the root directory with content similar to example [config.js](config.js).
 
+All options can be passed as command line (cli) arguments, which will override config file options.
 
 ### Usage
 Add this script to `package.json`:
--- config.js
+++ config.js
@@ -4,28 +4,38 @@ module.exports = function () {
     files: [ // list of source files to watch for changes and corresponding tasks to run
       {
         task: 'css', // name of the task to run, must be one of ['css', 'copy']
-        watch:  [ // file/s to watch for changes, using glob pattern
-          'style/**/*.less',
-          'style/override/**/*',
-          'style/theme.config'
+        watch: [ // file/s to watch for changes, using glob pattern
+          'style/*.less', // only match files in root folder, sub-folders should have their own watcher
+          'style/fonts/**/*.less',
         ],
-        compile: 'style/all.less', // file/s to compile when changes detected, using glob pattern
+        compile: 'style/_all.less', // file/s to compile when changes detected, using glob pattern
         output: 'public/static/', // destination directory where new file/s should be saved
+        renameOptions: {basename: 'all'}, // change output file name
       },
       {
         task: 'copy', // copy over `compile` file/s to `output` directory without compilation
-        watch:  [
-          'style/fonts/**/*', // watch for changes of all file types within `style/fonts/` directory
+        watch: [
+          'style/fonts/**/*.{eot,eof,svg,ttf,woff,woff2}', // watch for changes of all file types within `style/fonts/` directory
         ],
         compile: 'style/fonts/**/*.{eot,eof,svg,ttf,woff,woff2}', // only copy over files with matched extensions
         output: 'public/static/fonts/',
         renameOptions: {dirname: ''}, // make folder structure flat on output, using `gulp-rename` npm package
       },
+      /* Subtask to compile Semantic UI only to improve performance */
+      {
+        task: 'css',
+        watch: [
+          'style/override/**/*',
+        ],
+        compile: 'style/override/_semantic.less',
+        output: 'public/static/',
+        renameOptions: {basename: 'semantic'}, // change output file name
+      },
     ],
     symlinks: [ // useful for Semantic UI theme.config setup using `semantic-ui-less` library
       {
         // paths are relative to process.cwd()
-        target: 'style/theme.config', // path to the source file to reference
+        target: 'style/override/theme.config', // path to the source file to reference
         link: 'node_modules/semantic-ui-less/theme.config' // path to the file that references the target
       }
     ],
@@ -35,21 +45,24 @@ module.exports = function () {
     flexbugsfix: true,
     javascriptEnabled: true,
     browserslist: {
-      "production": [
-        ">0.3%",
-        "not dead",
-        "not op_mini all"
+      'production': [
+        '>0.3%',
+        'not dead',
+        'not op_mini all'
       ],
-      "development": [
-        "last 1 chrome version",
-        "last 1 firefox version",
-        "last 1 safari version",
-        "last 1 ie version"
+      'development': [
+        'last 1 chrome version',
+        'last 1 firefox version',
+        'last 1 safari version',
+        'last 1 ie version'
       ]
     },
+    postcssPlugins: [], // additional PostCSS plugins to use (you must install them yourself)
+    postcssOptions: undefined, // additional PostCSS options to use (i.e. `{ parser: 'sugarss' }`)
     plugins: [
       'less-plugin-glob',
       'less-plugin-functions',
     ],
+    // ...Other options passed to `lessc` compiler (currently using `gulp-less` npm package)
   }
 }
